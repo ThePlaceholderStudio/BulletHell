9119dd2761368c2912847d2caf4b9aa2a0de70cf
Mr-Clk <omer_celik@hacettepe.edu.tr>
2024-01-15 22:12:13 +0300

    Randomized traits on level up

--- Assets/Scripts/Items & Inventory/UI/Inventory.cs
+++ Assets/Scripts/Items & Inventory/UI/Inventory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Inventory : MonoBehaviour
 {
@@ -33,16 +34,61 @@ public class Inventory : MonoBehaviour
         AssignItemToSlots();
     }
 
+    public Item GetRandomItem(List<Item> items)
+    {
+        // Assign a rarity to each item.
+        foreach (EquippableItem item in items)
+        {
+            item.EquipmentRarity = (Rarity)Random.Range(0, 5);
+        }
+
+        // Assign a weight to each rarity.
+        int[] weights = new int[5] { 50, 30, 15, 4, 1 };
+
+        // Calculate the total weight of all the rarities.
+        int totalWeight = 0;
+        foreach (int weight in weights)
+        {
+            totalWeight += weight;
+        }
+
+        // Generate a random number between 0 and the total weight.
+        int randomNumber = Random.Range(0, totalWeight);
+
+        // Iterate through the list of items, adding up the weights of the rarities as you go.
+        int weightSum = 0;
+        foreach (EquippableItem item in items)
+        {
+            weightSum += weights[(int)item.EquipmentRarity];
+
+            // When the sum of the weights exceeds the random number, select the current item and return it.
+            if (weightSum > randomNumber)
+            {
+                return item;
+            }
+        }
+
+        // If the end of the list is reached without selecting an item, return the last item.
+        return items[items.Count - 1];
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("inv enb");
+
+        AssignItemToSlots();
+    }
+
     private void Update()
     {
         if (character.currentLevel % 5 == 0 && character.currentLevel <= 25)
         {
             AssignWeaponsToSlots();
         }
-        else
-        {
-            AssignItemToSlots();
-        }
+        //else
+        //{
+        //    AssignItemToSlots();
+        //}
     }
 
     private void AssignWeaponsToSlots()
@@ -64,7 +110,7 @@ public class Inventory : MonoBehaviour
         int i = 0;
         for (; i < items.Count && i < itemSlots.Length; i++)
         {
-            itemSlots[i].Item = items[i];
+            itemSlots[i].Item = GetRandomItem(items);
         }
 
         for (; i < itemSlots.Length; i++)
